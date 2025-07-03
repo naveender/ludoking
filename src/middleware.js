73617e058fd2d479f11047256fef0f1b47a5f67f
naveender.s <naveender.s@covrize.com>
2025-07-03 17:09:56 +0530

    udpates middleware

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export function middleware(request) {
   const session = request.cookies.get('session_user')?.value;
 
-  const authRequiredPaths = ['/battles', '/profile', '/my-wallet','/add-cash','/games-history','/room','/dashboard'];
+  const authRequiredPaths = [];
 
   if (authRequiredPaths.some(path => request.nextUrl.pathname.startsWith(path)) && !session) {
     return NextResponse.redirect(new URL('/login-register', request.url));
